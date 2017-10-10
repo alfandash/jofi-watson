e23492c0cc5220987c1a6538b05a9e049f798179
alfandash <cumiasem91@gmail.com>
2017-10-10 22:04:54 +0700

    redis for get job done

--- helper/getCityName.js
+++ helper/getCityName.js
@@ -2,9 +2,56 @@ const axios = require('axios')
 var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=-6.260898,106.7814405&sensor=true'
 
 
+var redis = require('redis')
+
+var client = redis.createClient()
+
+// Redis function to set redis
+const setCity = (latitude = '', longitude = '', city = '') => {
+  console.log('latitude ------------', latitude)
+  client.set('setCity' + latitude + longitude, city, 'EX', 1200)
+} 
+
 function getCityName (latitude, longitude, cb) {
   var googleMapApi = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&sensor=true&key=AIzaSyCsPckOoh98M3FLpimxQn5Hx4a_qB12uQs`
-  
+
+  // client.get('setCity' + latitude + longitude, function (err, replay) {
+  //   if (err) {
+  //     cb(err)
+  //   } else if (replay) {
+  //     console.log('redis api city google ------------', replay)
+  //     // return an redis
+  //     if (replay) {
+  //       console.log('masuk-------')
+  //       cb(replay)
+  //     } else {
+  //       cb('')
+  //     }
+  //   } else {
+  //     console.log('masuk sini--------------')
+  //     axios.get(googleMapApi)
+  //     .then(({ data }) => {
+  //       console.log('---------', data.results[0].address_components[7])
+  //       // set redis
+  //       if (data.results[0].address_components[7] !== undefined) {
+  //         setCity(latitude, longitude, data.results[0].address_components[7].short_name)
+  //       } else {
+  //         setCity(latitude, longitude, '')
+  //       }
+
+  //       cb(data.results[0].address_components[7].short_name)
+  //     })
+  //     .catch(error => {
+  //       // error 
+  //       cb(error.response)
+  //     })
+      
+  //   }
+  // })
+
+  // return {}
+
+
   var city = axios.get(googleMapApi)
   .then(({ data }) => {
     cb(data.results[0].address_components[7].short_name)
--- helper/getJob.js
+++ helper/getJob.js
@@ -1,10 +1,17 @@
 const axios = require('axios')
 var fastXmlParser = require('fast-xml-parser');
-
+var redis = require('redis')
 
 var papuas = require('../models/papua')
 var babels = require('../models/babel')
 var kepris = require('../models/kepri')
+
+var client = redis.createClient()
+
+// Redis function
+const setJobLocationExpert = (location = '',expert = '', obj) => {
+  client.set('getJob' + location + expert, JSON.stringify(obj), 'EX', 120)
+}
 
 var getJobList = (action, cb) => {
 
@@ -17,16 +24,37 @@ var getJobList = (action, cb) => {
   // console.log(specification)
   if (specification.location !== '' && specification.expert !== '') {
     const url = `https://stackoverflow.com/jobs/feed?q=${encodeURI(specification.expert)}&l=${encodeURI(specification.location)}&d=20&u=Km`
-    axios.get(url)
-      .then(result => {
-        var jsonString = fastXmlParser.parse(result.data);
-        // console.log(jsonString.rss.channel.item)
-        cb(jsonString.rss.channel.item)
-      })
-      .catch(err => {
-        console.log(err)
+    client.get('getJob' + specification.location + specification.expert, function (err, replay) {
+      if (err) {
         cb(err)
-      })
+      } else if (replay) {
+
+        // console.log('redis location dan expert------------', replay)
+        // return an redis
+        if (replay !== 'undefined') {
+          // console.log('masuk-------')
+          cb(JSON.parse(replay))
+        } else {
+          cb(undefined)
+        }
+
+      } else {
+        axios.get(url)
+        .then(result => {
+          var jsonString = fastXmlParser.parse(result.data);
+          // console.log(jsonString.rss.channel.item)\
+
+          // set redis
+          setJobLocationExpert(specification.location, specification.expert, jsonString.rss.channel.item)
+
+          cb(jsonString.rss.channel.item)
+        })
+        .catch(err => {
+          console.log(err)
+          cb(err)
+        })
+      }
+    })
   } else if (specification.location !== '') {
     // console.log('lokasi nih bra',specification.location)
     if (specification.location == 'Kepulauan Bangka Belitung') {
@@ -75,26 +103,90 @@ var getJobList = (action, cb) => {
       })
     } else {
       const locationUrl = `https://stackoverflow.com/jobs/feed?l=${encodeURI(specification.location)}%2c+Indonesia&d=20&u=Km`
-      axios.get(locationUrl)
-        .then(result => {
-          var jsonString = fastXmlParser.parse(result.data);
-          cb(jsonString.rss.channel.item)
-        })
-        .catch(err => {
+
+      client.get('getJob' + specification.location, function (err, replay) {
+        if (err) {
           cb(err)
-          // console.log(err)
-        })
+        } else if (replay) {
+
+          // console.log('redis location------------', replay)
+          // return an redis
+          if (replay !== 'undefined') {
+            // console.log('masuk-------')
+            cb(JSON.parse(replay))
+          } else {
+            cb(undefined)
+          }
+
+        } else {
+          axios.get(locationUrl)
+          .then(result => {
+            var jsonString = fastXmlParser.parse(result.data);
+
+            // set redis
+            setJobLocationExpert(specification.location, '', jsonString.rss.channel.item)
+
+            cb(jsonString.rss.channel.item)
+          })
+          .catch(err => {
+            console.log(err)
+            cb(err)
+          })
+        }
+      })
+      // axios.get(locationUrl)
+      //   .then(result => {
+      //     var jsonString = fastXmlParser.parse(result.data);
+      //     cb(jsonString.rss.channel.item)
+      //   })
+      //   .catch(err => {
+      //     cb(err)
+      //     // console.log(err)
+      //   })
     }
   } else if (specification.expert !== '') {
-    const url = `https://stackoverflow.com/jobs/feed?q=${encodeURI(specification.expert)}&l=indonesia&d=20&u=Km`
-    axios.get(url)
-      .then(result => {
-        var jsonString = fastXmlParser.parse(result.data);
-        cb(jsonString.rss.channel.item)
-      })
-      .catch(err => {
-        // console.log(err)
-      })
+    const urlExpert = `https://stackoverflow.com/jobs/feed?q=${encodeURI(specification.expert)}&l=indonesia&d=20&u=Km`
+
+    client.get('getJob' + specification.expert, function (err, replay) {
+      if (err) {
+        cb(err)
+      } else if (replay) {
+
+        // console.log('redis exoert------------', replay)
+        // return an redis
+        if (replay !== 'undefined') {
+          // console.log('masuk-------')
+          cb(JSON.parse(replay))
+        } else {
+          cb(undefined)
+        }
+
+      } else {
+        axios.get(urlExpert)
+          .then(result => {
+            var jsonString = fastXmlParser.parse(result.data);
+
+            // set redis
+            setJobLocationExpert('', specification.expert, jsonString.rss.channel.item)
+
+            cb(jsonString.rss.channel.item)
+          })
+          .catch(err => {
+            console.log(err)
+            cb(err)
+          })
+      }
+    })
+
+    // axios.get(urlExpert)
+    //   .then(result => {
+    //     var jsonString = fastXmlParser.parse(result.data);
+    //     console.log('replay get ob --------------', jsonString.rss.channel.item)
+    //     cb(jsonString.rss.channel.item)
+    //   })
+    //   .catch(err => {
+    //     console.log(err)
+    //   })
   } else {
     return cb({})
   }
